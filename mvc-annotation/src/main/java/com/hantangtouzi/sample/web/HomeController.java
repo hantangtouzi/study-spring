72ff842b571fea30148051861042fb5e5a7a8c39
WenLiang <hantangtouzi@foxmail.com>
2018-07-10 01:23:55 +0800

    void

--- mvc-annotation/src/main/java/com/hantangtouzi/sample/web/HomeController.java
+++ mvc-annotation/src/main/java/com/hantangtouzi/sample/web/HomeController.java
@@ -11,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/home", method = RequestMethod.GET)
     public String home() {
+        System.out.println("------------------------");
         return "home";
     }
 }
