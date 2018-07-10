9798653fd8bb37418b7c96be448d1cd4adcc5ef2
WenLiang <hantangtouzi@foxmail.com>
2018-07-10 23:47:33 +0800

    添加模块：mvc-thymeleaf

---
+++ mvc-thymeleaf/src/main/java/com/hantangtouzi/sample/config/MyWebApplicationInitializer.java
@@ -0,0 +1,29 @@
+package com.hantangtouzi.sample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/7/10 10:58
+ */
+
+@Configuration
+public class MyWebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        //return new Class[]{};
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+}
---
+++ mvc-thymeleaf/src/main/java/com/hantangtouzi/sample/config/RootConfig.java
@@ -0,0 +1,12 @@
+package com.hantangtouzi.sample.config;
+
+/**
+ * @author WilliamChang.
+ * @since 2018/7/10 0:43
+ */
+
+//@Configuration
+//@ComponentScan(basePackages = "com.hantangtouzi.sample")
+public class RootConfig {
+
+}
