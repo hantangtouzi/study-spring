2b73596c9e364ac4a072dc2b6601787c5043ecef
WenLiang <hantangtouzi@foxmail.com>
2018-07-12 17:49:14 +0800

    void

---
+++ spring-mvc-mybatis/src/main/java/com/hantangtouzi/sample/entity/Role.java
@@ -0,0 +1,35 @@
+package com.hantangtouzi.sample.entity;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/7/12 17:14
+ */
+
+public class Role {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ spring-mvc-mybatis/src/main/java/com/hantangtouzi/sample/web/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.hantangtouzi.sample.web.controller;
+
+import com.hantangtouzi.sample.entity.User;
+import com.hantangtouzi.sample.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/7/11 3:43
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/get/{id}")
+    public String getUserById(@PathVariable("id") Long id, Model model) {
+        User user = userService.getUserById(id);
+        System.out.println(user);
+        model.addAttribute("user", user);
+        return "userInfo";
+    }
+
+    @GetMapping("/list")
+    @ResponseBody
+    public List<User> list() {
+        return userService.findUsers();
+    }
+}
