41469ab43520cdb498867c8b137ea81f3a5af437
WenLiang <hantangtouzi@foxmail.com>
2018-07-12 23:20:42 +0800

    void

---
+++ security-web/src/main/java/com/hantangtouzi/sample/config/DataConfig.java
@@ -0,0 +1,45 @@
+package com.hantangtouzi.sample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/7/11 3:05
+ */
+
+@Configuration
+public class DataConfig {
+    //@Bean
+    //public DruidDataSource dataSource() {
+    //    DruidDataSource druidDataSource = new DruidDataSource();
+    //    druidDataSource.setDriverClassName("org.h2.Driver");
+    //    druidDataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+    //    druidDataSource.setUsername("sa");
+    //    druidDataSource.setPassword("");
+    //    druidDataSource.setInitialSize(5);
+    //    druidDataSource.setMaxActive(10);
+    //    return druidDataSource;
+    //}
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("classpath:schema.sql").addScript("classpath:test-data.sql").build();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate() {
+        return new JdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+}
---
+++ security-web/src/main/java/com/hantangtouzi/sample/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,11 @@
+package com.hantangtouzi.sample.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * @author WilliamChang.
+ * Created on 2018/7/12 19:23
+ */
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
