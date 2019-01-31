75dad20ce9ab8b8db8e65810ed6d5c7ed5288ac3
hantangtouzi <hantangtouzi@foxmail.com>
2019-02-01 00:30:19 +0800

    void

--- data-jdbc/src/main/java/com/hantangtouzi/datajdbc/config/DataConfig.java
+++ data-jdbc/src/main/java/com/hantangtouzi/datajdbc/config/DataConfig.java
@@ -1,7 +1,6 @@
 package com.hantangtouzi.datajdbc.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
@@ -14,7 +13,7 @@ import javax.sql.DataSource;
  * Created on 2018/7/11 3:05
  */
 
-@Configuration
+// @Configuration
 public class DataConfig {
     //@Bean
     //public DruidDataSource dataSource() {
--- data-jdbc/src/main/java/com/hantangtouzi/datajdbc/config/RootConfig.java
+++ data-jdbc/src/main/java/com/hantangtouzi/datajdbc/config/RootConfig.java
@@ -1,17 +1,13 @@
 package com.hantangtouzi.datajdbc.config;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-
 /**
  * @author WilliamChang.
  * @since 2018/7/10 0:43
  */
 
-@Configuration
-@Import(DataConfig.class)
-@ComponentScan(basePackages = "com.hantangtouzi.datajdbc")
+// @Configuration
+// @Import(DataConfig.class)
+// @ComponentScan(basePackages = "com.hantangtouzi.datajdbc")
 public class RootConfig {
 
 }
