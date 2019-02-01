f4a478f625ac3f8c8217b974bae2493f0d1b032a
hantangtouzi <hantangtouzi@foxmail.com>
2019-02-01 12:19:02 +0800

    void

---
+++ data-cache/src/main/java/com/hantangtouzi/datacache/config/Config.java
@@ -0,0 +1,9 @@
+package com.hantangtouzi.datacache.config;
+
+/**
+ * @author WilliamChang.
+ * Created on 2019-02-01 12:16:27
+ */
+
+public class Config {
+}
---
+++ data-cache/src/main/java/com/hantangtouzi/datacache/dao/UserDao.java
@@ -0,0 +1,61 @@
+package com.hantangtouzi.datacache.dao;
+
+import com.hantangtouzi.datacache.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author WilliamChang.
+ * Created on 2019-02-01 12:17:48
+ */
+
+@Repository
+public class UserDao {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+    public User getUserById(Long id) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        return namedParameterJdbcTemplate.queryForObject("select * from t_user where id = :id", map, new UserRowMapper());
+    }
+
+    public List<User> findUsers() {
+        return jdbcTemplate.query("select * from t_user", new UserRowMapper());
+    }
+
+    public void addUser(User user) {
+        jdbcTemplate.update("insert into t_user(id, username, password) values (?, ? , ?)", user.getId(), user.getUsername(), user.getPassword());
+    }
+
+    public void deleteUserById(Long userId) {
+        jdbcTemplate.update("delete from t_user where id = ?", userId);
+    }
+
+    public void updateUser(User user) {
+        jdbcTemplate.update("update t_user set username = ?, password = ? where id = ?", user.getUsername(), user.getPassword(), user.getId());
+    }
+
+    private class UserRowMapper implements RowMapper<User> {
+        @Override
+        public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+            User user = new User();
+            user.setId(rs.getInt("id"));
+            user.setUsername(rs.getString("username"));
+            user.setPassword(rs.getString("password"));
+            return user;
+        }
+    }
+}
